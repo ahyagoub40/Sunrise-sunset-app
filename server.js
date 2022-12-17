65febe525f47e99601e7feda00e39e58a6c7dda4
Ahmed Yagoub <ahyagoub40@gmail.com>
2022-12-17 15:07:48 -0700

    Get data from public api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 let sunsetTime;
 let sunriseTime;
+let index = 0;
+let list;
 fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
   .then((response) => response.json())
   .then((data) => {
@@ -8,6 +10,34 @@ fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=to
     sunsetTime = sunset.split(':', 2).join(':')
     sunriseTime = sunrise.split(':', 2).join(':')
     console.log({sunsetTime, sunriseTime})
-    document.querySelector('.time').innerHTML = `${sunriseTime} AM`
+    list = [
+      {
+        timeOfDay: 'Sunrise',
+        time: sunriseTime,
+        ampm: 'AM'
+      },
+      {
+        timeOfDay: 'Sunset',
+        time: sunsetTime,
+        ampm: 'PM'
+      }
+    ]
+    
+    
+    typeToHtml()
   }
-    );
+  );
+  function switchTimes () {
+    if (index === 0) {
+      index = 1
+    } else  {
+      index = 0
+    }
+    typeToHtml()
+  }
+  
+  function typeToHtml () {
+    document.querySelector('.time-description').innerHTML = list[index].timeOfDay
+    document.querySelector('.time').innerHTML = `${list[index].time} ${list[index].ampm}`
+  }
+  

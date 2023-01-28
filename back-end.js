c2f8a78fee95af1a9d6da5aadc57a91fb71678ce
mustafa <mustafa.bhm3@gmail.com>
2023-01-28 15:50:56 -0700

    updating back-end

--- back-end.js
+++ back-end.js
@@ -6,4 +6,18 @@ const supabaseAnonKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-supabase.from("sunData2").select("*").then(console.log).catch(console.error);
+// supabase.from("sunData2").select("*").then(console.log).catch(console.error);
+
+const main = async () => {
+  let { data, error } = await supabase.from("sunData2").select("*").limit(5);
+
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const sunrise = data[0].sunrise;
+  const sunset = data[0].sunset;
+  console.log("sunrise", sunrise);
+  console.log("sunset", sunset);
+};
+main();
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseAnonKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 let sunsetTime;
 let sunriseTime;
 let risecountDown;
@@ -8,56 +16,128 @@ let todayDate;
 let timeObj;
 let index = 1;
 let list;
-fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
+fetch(
+  "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today"
+)
   .then((response) => response.json())
   .then((data) => {
-    const sunrise = data.results.sunrise
-    const sunset = data.results.sunset
-    sunsetTime = sunset.split(':', 2).join(':')
-    sunriseTime = sunrise.split(':', 2).join(':')
-    console.log({ sunsetTime, sunriseTime })
-    let riseTimeArr = sunrise.split(':', 3);
-    let setTimeArr = sunset.split(':', 3);
+    const sunrise = data.results.sunrise;
+    const sunset = data.results.sunset;
+    console.log("sunrise", sunrise);
+    console.log("sunset", sunset);
+    sunsetTime = sunset.split(":", 2).join(":"); // should be a string format
+    sunriseTime = sunrise.split(":", 2).join(":");
+
+    let riseTimeArr = sunrise.split(":", 3);
+    let setTimeArr = sunset.split(":", 3);
     todayDate = new Date();
     // syntax: new Date(year, monthIndex, day, hours, minutes, seconds)
-    riseTimeObj = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate(), riseTimeArr[0], riseTimeArr[1], riseTimeArr[2].split(" ")[0]);
-    setTimeObj = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate(), setTimeArr[0], setTimeArr[1], setTimeArr[2].split(" ")[0]);
+    riseTimeObj = new Date(
+      todayDate.getFullYear(),
+      todayDate.getMonth(),
+      todayDate.getDate(),
+      riseTimeArr[0],
+      riseTimeArr[1],
+      riseTimeArr[2].split(" ")[0]
+    );
+    setTimeObj = new Date(
+      todayDate.getFullYear(),
+      todayDate.getMonth(),
+      todayDate.getDate(),
+      setTimeArr[0],
+      setTimeArr[1],
+      setTimeArr[2].split(" ")[0]
+    );
 
-    console.log("rise= ", riseTimeObj, "set= ", setTimeObj);
     list = [
       {
-        timeOfDay: 'Sunrise',
+        timeOfDay: "Sunrise",
         time: sunriseTime,
-        ampm: 'AM'
+        ampm: "AM",
       },
       {
-        timeOfDay: 'Sunset',
+        timeOfDay: "Sunset",
         time: sunsetTime,
-        ampm: 'PM'
-      }
-    ]
-    typeToHtml()
+        ampm: "PM",
+      },
+    ];
+    typeToHtml();
+  });
+
+/// fetch from database
+
+const main = async () => {
+  let { data, error } = await supabase.from("sunData2").select("*").limit(5);
+
+  if (error) {
+    console.log(error);
+    return;
   }
+  const sunrise = data[0].sunrise;
+  const sunset = data[0].sunset;
+  console.log("sunrise", sunrise);
+  console.log("sunset", sunset);
+  sunsetTime = sunset.split(":", 2).join(":"); // should be a string format
+  sunriseTime = sunrise.split(":", 2).join(":");
+
+  let riseTimeArr = sunrise.split(":", 3);
+  let setTimeArr = sunset.split(":", 3);
+  todayDate = new Date();
+  // syntax: new Date(year, monthIndex, day, hours, minutes, seconds)
+  riseTimeObj = new Date(
+    todayDate.getFullYear(),
+    todayDate.getMonth(),
+    todayDate.getDate(),
+    riseTimeArr[0],
+    riseTimeArr[1],
+    riseTimeArr[2].split(" ")[0]
   );
+  setTimeObj = new Date(
+    todayDate.getFullYear(),
+    todayDate.getMonth(),
+    todayDate.getDate(),
+    setTimeArr[0],
+    setTimeArr[1],
+    setTimeArr[2].split(" ")[0]
+  );
+
+  list = [
+    {
+      timeOfDay: "Sunrise",
+      time: sunriseTime,
+      ampm: "AM",
+    },
+    {
+      timeOfDay: "Sunset",
+      time: sunsetTime,
+      ampm: "PM",
+    },
+  ];
+  typeToHtml();
+};
+main();
+
 function switchTimes() {
   if (index === 0) {
-    index = 1
-    document.querySelector('.sun').style.animation = "backwards sun-animate-reverse 3s "
-    document.querySelector('.main-gradient').style.animation = "backwards gradient-reverse 3s"
+    index = 1;
+    document.querySelector(".sun").style.animation =
+      "backwards sun-animate-reverse 3s ";
+    document.querySelector(".main-gradient").style.animation =
+      "backwards gradient-reverse 3s";
   } else {
-    index = 0
-    document.querySelector('.sun').style.animation = "forwards sun-animate 3s"
-    document.querySelector('.main-gradient').style.animation = "forwards gradient 3s"
+    index = 0;
+    document.querySelector(".sun").style.animation = "forwards sun-animate 3s";
+    document.querySelector(".main-gradient").style.animation =
+      "forwards gradient 3s";
   }
   clearInterval(interval);
   interval = setInterval(countdown, 1000, index);
-  typeToHtml()
+  typeToHtml();
 }
 
 function countdown(index) {
   todayDate = new Date().getTime();
   console.log("now", todayDate);
-
 
   let word;
   if (index === 1) {
@@ -66,25 +146,31 @@ function countdown(index) {
     // console.log(riseCountDown.toString());
     let timeObj = new Date(riseCountDown);
     var days = Math.floor(riseCountDown / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((riseCountDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (riseCountDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((riseCountDown % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((riseCountDown % (1000 * 60)) / 1000);
 
     word = "sunrise";
-    document.getElementById("countdownField").innerHTML = hours + ":" + minutes + ":" + seconds + " to " + word;
+    document.getElementById("countdownField").innerHTML =
+      hours + ":" + minutes + ":" + seconds + " to " + word;
   } else {
-    console.log('count to sunset');
+    console.log("count to sunset");
     // setCountDown -= 1000;
     setCountDown = setTimeObj.getTime() - todayDate;
-    console.log('count to rise');
+    console.log("count to rise");
     // console.log(setCountDown.toString());
     let timeObj = new Date(setCountDown);
     var days = Math.floor(setCountDown / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((setCountDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (setCountDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((setCountDown % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((setCountDown % (1000 * 60)) / 1000);
     word = "sunset";
-    document.getElementById("countdownField").innerHTML = hours + ":" + minutes + ":" + seconds + " to " + word;
+    document.getElementById("countdownField").innerHTML =
+      hours + ":" + minutes + ":" + seconds + " to " + word;
   }
 
   // if (riseCountDown < 0 || setCountDown < 0) {
@@ -93,7 +179,8 @@ function countdown(index) {
   // }
 }
 function typeToHtml() {
-  document.querySelector('.time-description').innerHTML = list[index].timeOfDay
-  document.querySelector('.time').innerHTML = `${list[index].time} ${list[index].ampm}`
+  document.querySelector(".time-description").innerHTML = list[index].timeOfDay;
+  document.querySelector(
+    ".time"
+  ).innerHTML = `${list[index].time} ${list[index].ampm}`;
 }
-
